9231acf0abfa079e0c19d44045b443e09deddecb
Göktuğ <gokojapan5@gmail.com>
2022-12-13 16:25:21 +0300

    Project started

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TRMap from './components/TRMap.jsx';
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center">
+      <div className="w-screen">
+        <TRMap />
+      </div>
+    </div>
+  );
+}
+
+export default App;
